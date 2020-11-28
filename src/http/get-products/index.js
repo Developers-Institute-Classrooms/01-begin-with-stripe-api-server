8eb797299150e5c23a8c59008c870b57f205ca69
Lance Cooper <lwcooper@gmail.com>
2020-11-28 19:02:06 +1300

    Add helpful error

--- src/http/get-products/index.js
+++ src/http/get-products/index.js
@@ -2,18 +2,35 @@ const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
+const response = {
+  headers: {
+    "content-type": "application/json; charset=utf8",
+    "cache-control":
+      "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+  },
+};
+
 exports.handler = async function products(req) {
+  if (!STRIPE_SECRET_KEY) {
+    return {
+      ...response,
+      statusCode: 500,
+      body: JSON.stringify({
+        errors: [
+          {
+            name: "Missing Stripe Key",
+          },
+        ],
+      }),
+    };
+  }
   const products = await stripe.products.list({
     limit: 3,
   });
 
   return {
+    ...response,
     statusCode: 201,
-    headers: {
-      "content-type": "application/json; charset=utf8",
-      "cache-control":
-        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
-    },
     body: JSON.stringify({
       products,
     }),
